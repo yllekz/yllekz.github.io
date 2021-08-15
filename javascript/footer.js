40065eaa92454e240541bdb0d78201c1f53574b9
yllekz <steve.g.kelly@me.com>
2021-08-14 20:11:32 -0400

    CSS tweaks -Light/dark themes -Remove some obsolete stuff
    Fix copyright date

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,5 +1,5 @@
 document.write('<span class="jsfooter">');
 
-document.write('Copyright 2005-2020, cuz I copyright stuff');
+document.write('Copyright 2005-[current year], cuz I copyright stuff');
 
 document.write('</span>');
